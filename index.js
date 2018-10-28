96e3cd7e7068f95981b901fe73cf028440117f94
anu-007 <anubhutiv1@gmail.com>
2018-10-29 01:47:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+/*
+ * Primary file for tool
+ *
+ */
+
+// Dependencies
+const http = require('http');
+
+ // Configure the server to respond to all requests with a string
+const server = http.createServer(function(req,res){
+  res.end('Hello World!\n');
+});
+
+// Start the server
+server.listen(3000,function(){
+  console.log('The server is up and running now');
+});
